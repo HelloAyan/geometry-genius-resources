c532a21fac2da9f21cf0344c87cc71033301eb70
HelloAyan <helloayan171@gmail.com>
2023-02-19 23:54:45 +0600

    parallelogramResult calculation completed

--- js/script.js
+++ js/script.js
@@ -32,14 +32,37 @@ document.getElementById('rectangleCalculation').addEventListener('click', functi
     const rectangleWField = document.getElementById('rectangleW');
     const newRectangleWFieldString = rectangleWField.value;
     const newRectangleWField = parseFloat(newRectangleWFieldString);
+    rectangleWField.value = '';
 
     const rectangleLField = document.getElementById('rectangleL');
     const newRectangleLFieldString = rectangleLField.value;
     const newRectangleLField = parseFloat(newRectangleLFieldString);
+    rectangleLField.value = '';
 
     const RectAngleFinalResult = newRectangleWField * newRectangleLField;
 
 
     const rectangleFinalResultField = document.getElementById('rectangleFinalResult');
     rectangleFinalResultField.innerText = RectAngleFinalResult;
+})
+
+
+// parallelogram Calculation Start
+
+document.getElementById('parallelogramCalculation').addEventListener('click', function () {
+    const parallelogramBField = document.getElementById('parallelogramB');
+    const newParallelogramBFieldString = parallelogramBField.value;
+    const newParallelogramBField = parseFloat(newParallelogramBFieldString);
+    parallelogramBField.value = '';
+
+
+    const parallelogramHField = document.getElementById('parallelogramH');
+    const newParallelogramHFieldString = parallelogramHField.value;
+    const newParallelogramHField = parseFloat(newParallelogramHFieldString);
+    parallelogramHField.value = '';
+
+    const parallelogramResult = newParallelogramBField * newParallelogramHField;
+
+    const parallelogramFinalResultField = document.getElementById('parallelogramFinalResult');
+    parallelogramFinalResultField.innerText = parallelogramResult;
 })
